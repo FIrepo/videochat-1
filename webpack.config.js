4867c8b789222c175e9e9b1cc8d8a34f53b77117
Oleg Orlov <oleg.orlv@gmail.com>
2017-03-16 03:08:20 +0300

    updated babel

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
         exclude: /node_modules/,
         loader: "babel-loader",
         query: {
-          presets: ['es2015','react']
+          presets: ['env','react']
         }
       },
       { test: /\.css$/, loader: "style-loader!css-loader" }
